35da3ce10fb7605302d28f4b5ef62adb92305e9e
alsebb <alessandrosebastiani172@gmail.com>
2022-12-09 19:19:30 +0100

    feat: new template, different anchors

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,7 +230,7 @@
     });
 
     // Add smooth scrolling on all links inside the price cards
-    $("#Pricing a").on("click", function (event) {
+    $("#piani a").on("click", function (event) {
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
         // Prevent default anchor click behavior
